8ee4a9515c90ce96f87930af7ca069a839e00006
tibo29 <tibo.vandenberg@student.uhasselt.be>
2023-01-09 21:56:32 +0100

    addEtappeMeteenNaWedstrijdController + fxml toegevoegd

--- project-template-202223/src/main/java/be/kuleuven/vrolijkezweters/controller/addEtappeMeteenNaWedstrijdController.java
+++ project-template-202223/src/main/java/be/kuleuven/vrolijkezweters/controller/addEtappeMeteenNaWedstrijdController.java
@@ -1,50 +1,46 @@
 package be.kuleuven.vrolijkezweters.controller;
 
+import be.kuleuven.vrolijkezweters.Etappe;
 import be.kuleuven.vrolijkezweters.ProjectMain;
 import be.kuleuven.vrolijkezweters.Wedstrijden;
 import be.kuleuven.vrolijkezweters.databaseConnection;
+import com.sun.javafx.image.IntPixelGetter;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.LightBase;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import org.jdbi.v3.core.Handle;
 
 import java.util.List;
 
-public class addEtappeController {
+public class addEtappeMeteenNaWedstrijdController {
 
     @FXML
-    private TextField WedstrijdId;
+    private TextField BeginLocatie2;
     @FXML
-    private TextField EtappeNummer;
+    private TextField EindLocatie2;
     @FXML
-    private TextField BeginLocatie;
+    private Button addEtappe2;
     @FXML
-    private TextField EindLocatie;
-    @FXML
-    private Button addEtappe;
-    @FXML Button btnCloseAddEtappe;
+    private Text etappeText2;
+
 
     private List<Wedstrijden> list;
 
-    public void initialize() {
-        addEtappe.setOnAction(e -> addEtappe());
-        btnCloseAddEtappe.setOnAction(e -> {
-            var stage = (Stage) btnCloseAddEtappe.getScene().getWindow();
-            stage.close();
-            showPreviousWindow();
-        });
-    }
-    private void addEtappe() {
+    private int i = 1;
 
-        float etappeNummer = Integer.parseInt(EtappeNummer.getText());
-        float beginLocatie = Float.parseFloat(BeginLocatie.getText());
-        float eindLocatie = Float.parseFloat(EindLocatie.getText());
+    public void initialize() {
+        addEtappe2.setOnAction(e -> addEtappe2());
+    }
+    private void addEtappe2() {
+
+        float beginLocatie = Float.parseFloat(BeginLocatie2.getText());
+        float eindLocatie = Float.parseFloat(EindLocatie2.getText());
         float etappeAfstand = eindLocatie-beginLocatie;
 
         databaseConnection databaseConnection = new databaseConnection();
@@ -55,22 +51,32 @@ public class addEtappeController {
         int wedstrijdId = list.get(0).getWedstrijdId();
         int aantalEtappes = Integer.parseInt(list.get(0).getAantalEtappes());
 
-        var stage = (Stage) addEtappe.getScene().getWindow();
+        var stage = (Stage) addEtappe2.getScene().getWindow();
 
-        handle.execute("INSERT INTO etappe (wedstrijdId, etappeNummer, beginLocatie, eindLocatie, etappeAfstand) VALUES (?, ?, ?, ?, ?)", wedstrijdId, etappeNummer, beginLocatie, eindLocatie, etappeAfstand);
-        handle.close();
-        stage.close();
-        showPreviousWindow();
+        if(i==aantalEtappes){
+            handle.execute("INSERT INTO etappe (wedstrijdId, etappeNummer, beginLocatie, eindLocatie, etappeAfstand) VALUES (?, ?, ?, ?, ?)", wedstrijdId, i, beginLocatie, eindLocatie, etappeAfstand);
+            handle.close();
+            stage.close();
+            refreshPreviousScene();
+        } else {
+            etappeText2.setText("Etappe " + (i + 1));
+            i++;
+            handle.execute("INSERT INTO etappe (wedstrijdId, etappeNummer, beginLocatie, eindLocatie, etappeAfstand) VALUES (?, ?, ?, ?, ?)", wedstrijdId, i, beginLocatie, eindLocatie, etappeAfstand);
+            BeginLocatie2.clear();
+            EindLocatie2.clear();
+        }
+
+
     }
 
-    private void showPreviousWindow(){
-        var resourceName = "beheeretappes.fxml";
+    private void refreshPreviousScene(){
+        var resourceName = "beheerwedstrijden.fxml";
         try {
             var stage = new Stage();
             var root = (AnchorPane) FXMLLoader.load(getClass().getClassLoader().getResource(resourceName));
             var scene = new Scene(root);
             stage.setScene(scene);
-            stage.setTitle("Admin etappe");
+            stage.setTitle("Admin wedstrijden");
             stage.initOwner(ProjectMain.getRootStage());
             stage.initModality(Modality.WINDOW_MODAL);
             stage.show();
@@ -79,5 +85,4 @@ public class addEtappeController {
             throw new RuntimeException("Kan beheerscherm " + resourceName + " niet vinden", e);
         }
     }
-
 }
